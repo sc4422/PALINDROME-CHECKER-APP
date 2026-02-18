02335c014a2adf619ca25919c18425800745c5c6
sc4422 <sc4422@srmist.edu.in>
2026-02-18 10:01:34 +0530

    UC2-added-half loop

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Palindromecheckerapp {
     public static void main (String[] args){
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter string: ");
+                String s = sc.next();
 
-    }
-}
+                int n = s.length();
+                boolean isPalindrome = true;
+
+                for (int i = 0; i < n/2; i++) {
+                    if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrome)
+                    System.out.println("Palindrome");
+                else
+                    System.out.println("Not Palindrome");
+
+                sc.close();
+            }
+        }
+
