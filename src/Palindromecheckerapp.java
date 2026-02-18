0b363b6fcd34a17cfb4a973eb2d556f32df15ccb
sc4422 <sc4422@srmist.edu.in>
2026-02-18 09:22:36 +0530

    UC2:Hardcoded palindrome

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Palindromecheckerapp {
-    public static void main(String[] args){
-        System.out.print("Welcome to palindrome checker management system");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String str = input.replaceAll("\\s+", "").toLowerCase();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        scanner.close();
     }
 }
